8ba39676e1cdd8c660fec35ac1b18b58222c3fe9
Nicko Arellano <99542997+NickoTheBot@users.noreply.github.com>
2022-03-21 12:43:25 -0400

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class reverse{
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter a string");
+		String str = scan.nextLine();
+		for(int i = (str.length() -1); i>=0; i--){
+			char letter = str.charAt(i);
+			System.out.printf("%s",letter);
+
+		}
+	}
+}
